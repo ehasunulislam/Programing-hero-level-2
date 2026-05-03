997da2bfd67067877d7564e98d31db3e79617086
ehasun15019 <ehasunulislam0202@gmail.com>
2026-05-03 09:48:32 +0600

    learn how to work generic

---
+++ typescript/module-2-Ts-Advance-Type/src/generic.ts
@@ -0,0 +1,11 @@
+type genericArray<T> = Array<T>
+
+// const name: string[] = ["ehasun", "fahim", "suvo"];
+const names: genericArray<string> =  ["ehasun", "fahim", "suvo"];
+
+// const contact: number[] = [111454, 125705, 12451];
+const contacts: genericArray<number> = [111454, 125705, 12451];
+
+
+// const eligible: boolean[] = [true, false, false, true]
+const eligibles: genericArray<boolean> = [true, false, false, true]
