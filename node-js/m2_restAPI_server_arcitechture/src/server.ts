939622229e3a073dca8d95dbd574e5c220d22d6f
ehasun15019 <ehasunulislam0202@gmail.com>
2026-05-11 20:38:12 +0600

    how to code in node.js

--- node-js/m2_restAPI_server_arcitechture/src/server.ts
+++ node-js/m2_restAPI_server_arcitechture/src/server.ts
@@ -1,7 +1,23 @@
 import { createServer, IncomingMessage, Server } from "http";
 
 const server: Server = createServer((req: IncomingMessage, res) => {
-    console.log(req)
+    // console.log(req)
+    
+    const url = req.url;
+    const method = req.method;
+
+    if(url === "/" && method === "GET") {
+        // console.log("This is root route");
+        res.writeHead(200, {"content-type": "application/json"})
+        res.end(JSON.stringify({ message: "This is root route" }))
+    } else if(url?.startsWith("/product")){
+        res.writeHead(200, {"content-type": "application/json"})
+        res.end(JSON.stringify({ message: "This is product route" }))
+    }
+    else {
+        res.writeHead(200, {"content-type": "application/json"})
+        res.end(JSON.stringify({ message: "This is root route" }))
+    }
 })
 
 server.listen(5000, () => {
